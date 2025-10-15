458b5034b6cde6a20a29404f68a1b19937f06633
Joshua <payoranyoshua@gmail.com>
2025-10-16 01:31:51 +0800

    Change stateless to stateful. Add a button, array of string, and text widget

--- flutter_activity_03/lib/pages/home.dart
+++ flutter_activity_03/lib/pages/home.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> phrases = [
+    'Git commit -m "nagkaon kana lab?"',
+    "Kape una, code sunod ☕",
+    "Programmer by day, bug hunter by night 🐛",
+    "Ctrl + c & v lang ang baon 😎",
+    "Mag contractor nalang ko sa DPWH 🚧",
+  ];
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text(
+          'Activity 3',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              phrases[index],
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  index = (index + 1) % phrases.length; 
+                });
+              },
+              child: const Text('Click Me'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
